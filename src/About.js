4deecb19d32453e62e6700523ac019b109ee63f6
Anastaciakuron <69893739+Anastaciakuron@users.noreply.github.com>
2021-12-01 21:32:46 +0700

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from './components/Navigation';
+
+
+const About = () => {
+  return(
+    <>
+      <Navigation />
+
+      <div className="container my-5">
+        <h1>About this Application</h1>
+        <p>Kuron, Anastacia syallomita sastaviana (105011910062). Program Studi : Sistem Informasi</p>
+      </div>
+
+    </>
+  );
+}
+
+export default About;
